412aaf721b6e5a7840122bce301611d669d91ee7
PeterIngrid <pingrid@gmx.de>
2020-08-26 14:59:08 +0200

    Eclipse Projekt angelegt

---
+++ Miyagis-Rezepteecke/src/Main.java
@@ -0,0 +1,18 @@
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+
+}
