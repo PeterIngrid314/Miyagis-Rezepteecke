8d97c4dc2a2491f05f4c211621c005a21d04197b
PeterIngrid <pingrid@gmx.de>
2020-08-27 19:03:52 +0200

    added android dev-kit

--- Miyagis-Rezepteecke/src/application/Main.java
+++ Miyagis-Rezepteecke/src/application/Main.java
@@ -4,14 +4,13 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
@@ -22,6 +21,6 @@ public class Main extends Application {
 	}
 	
 	public static void main(String[] args) {
-		launch(args); 
+		launch(args);
 	}
 }
